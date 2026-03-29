8c7b819593c5aac27fe3b55e71d6a29e561cafe3
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-03-29 20:16:56 -0300

    feat: basic css settings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 
+	//import de arquivos css
+	import "$lib/styles/neoreset.css";
+	import "$lib/styles/reset.css";
+	import "$lib/styles/main.css";
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_outward" />
 </svelte:head>
 
 {@render children()}
