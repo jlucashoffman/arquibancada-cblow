48dde3e23b776fe0a03ef4d1f568aa3e59059f8d
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-04 13:20:12 -0300

    feat: create component to show advices

---
+++ src/lib/components/advice.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let {message}:{message: string} = $props()
+</script>
+
+<p class="panel_style">{message}</p>
+
+<style>
+    p {
+        display: flex;
+        justify-content: center;
+        box-sizing: border-box;
+        padding: 8px 32px;
+        border-radius: 8px;
+        max-width: 600px;
+    }
+</style>
