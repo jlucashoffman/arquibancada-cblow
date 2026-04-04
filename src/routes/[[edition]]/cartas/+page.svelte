e8c9b25d205b7530f36b824be06b2e7d7bdbc69a
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-04 13:20:27 -0300

    feat: cards route

---
+++ src/routes/[[edition]]/cartas/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Advice from "$lib/components/advice.svelte";
+    import Deck from "$lib/components/layouts/deck.svelte";
+</script>
+
+
+
+<section id="cards_deck">
+    <div class="content_wrapper">
+    <Advice message="As imagens em breve serão adicionadas!" />
+        <Deck />
+    </div>
+</section>
+
+<style>
+    #cards_deck {
+    display: block;
+    width: 100%;
+
+    & .content_wrapper {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 16px;
+        margin: 0 auto;
+        margin-top: calc(128px);
+    }
+}
+</style>
