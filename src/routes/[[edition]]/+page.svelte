d50cb68133acc19a52fa720e41bf47f9f05db203
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-04 21:03:49 -0300

    fix: build issue

--- src/routes/[[edition]]/+page.svelte
+++ src/routes/[[edition]]/+page.svelte
@@ -1,16 +1,10 @@
 <script lang="ts">
     import LivePreview from "$lib/components/layouts/livePreview/component.svelte";
     import Participants from "$lib/components/layouts/participants.svelte";
-    import { progressManager } from "$lib/progress.svelte";
-    import Loading from "$lib/components/loading.svelte";
     import MatchHistory from "$lib/components/layouts/matchHistory.svelte";
 </script>
 
 <LivePreview />
-{#if progressManager.dataLoaded}
 <Participants />
 <h2>Chaveamento em breve</h2>
 <MatchHistory />
-{:else}
-<Loading />
-{/if}
