11e0c54056169e13b8385bde87575c4430d138ef
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-05 19:21:47 -0300

    chore: clear main page

--- src/routes/[[edition]]/+page.svelte
+++ src/routes/[[edition]]/+page.svelte
@@ -1,10 +1,2 @@
 <script lang="ts">
-    import LivePreview from "$lib/components/layouts/livePreview/component.svelte";
-    import Participants from "$lib/components/layouts/participants.svelte";
-    import MatchHistory from "$lib/components/layouts/matchHistory.svelte";
 </script>
-
-<LivePreview />
-<Participants />
-<h2>Chaveamento em breve</h2>
-<MatchHistory />
