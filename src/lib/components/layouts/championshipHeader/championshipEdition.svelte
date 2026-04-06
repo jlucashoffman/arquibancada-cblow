a4b453f7badf959bfe5266f3df464f6111ae7aa7
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-06 03:17:31 -0300

    feat: add championship header and edition components for improved layout

---
+++ src/lib/components/layouts/championshipHeader/championshipEdition.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Element from "$lib/components/element.svelte";
+</script>
+<Element idName="edition_controller">
+    <button id="change_edition" type="button">{`Edição 2026`}</button>
+</Element>
+
+<style>
+    :global(#edition_controller) {
+    &._vars {
+    --border-color: var(--color-element-interactive-border-color);
+    --border-radius: 8px;
+    --border-weight: 1px;
+    }
+
+    width: fit-content;
+
+    position: absolute;
+    top: 32px;
+    left: 16px;
+
+    & button#change_edition { padding: 8px 16px; } 
+
+    & ._background { background: var(--color-element-interactive-bg); }
+    }
+</style>
---
+++ src/lib/components/layouts/championshipHeader/championshipHeader.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Element from "$lib/components/element.svelte";
+    import BackgroundLive from "./backgroundLive.svelte";
+    import ChampionshipEdition from "./championshipEdition.svelte";
+
+</script>
+<Element idName="championship_header">
+    <BackgroundLive />
+    <ChampionshipEdition />
+    <h1>arquibancada<br/>cblow</h1>
+</Element>
+
+<style>
+    :global(#championship_header) {
+    width: 100%;
+
+    & h1 {
+    position: absolute;
+
+    bottom: 16px;
+    left: 16px;
+
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 3rem;
+    }
+    }
+</style>
