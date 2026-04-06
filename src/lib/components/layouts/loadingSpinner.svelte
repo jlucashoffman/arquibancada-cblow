eab5cdd47fd86545d9a90b566a42d9998b13a6b7
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-06 03:59:59 -0300

    feat: add loading spinner component and define spinner color variables

---
+++ src/lib/components/layouts/loadingSpinner.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import Element from "$lib/components/element.svelte";
+</script>
+
+<Element idName="loadingSpinner">
+	<span class="spinner"></span>
+</Element>
+
+<style>
+	:global(#loadingSpinner) {
+
+	& .spinner {
+	display: flex;
+
+	width: calc(1em * sqrt(2) - 4px); /* cálculo da diagonal subtraindo do tamanho das bordas */
+
+	aspect-ratio: 1;
+
+	box-sizing: border-box;
+	border-radius: 50%;
+	border: 2px solid var(--color-spinner-background);
+	border-top-color: var(--color-spinner-highlight);
+
+	animation: spin 0.8s linear infinite;
+	}}
+
+	@keyframes spin {
+	to { transform: rotate(1turn); }}
+
+	@media (prefers-reduced-motion: reduce) {
+	:global(#small_loading) .spinner {
+	animation: none;
+	opacity: 1;
+	}}
+</style>
