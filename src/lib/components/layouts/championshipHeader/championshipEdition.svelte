5e10e048f1e913a4d904728a9b4075a18c128a9f
J. Lucas Hoffman <contato.jlhoffman@gmail.com>
2026-04-06 04:00:45 -0300

    feat: update edition button to display loading spinner when data is unavailable

--- src/lib/components/layouts/championshipHeader/championshipEdition.svelte
+++ src/lib/components/layouts/championshipHeader/championshipEdition.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
     import Element from "$lib/components/element.svelte";
     import { gameMaster } from "$lib/database/data.svelte";
+    import LoadingSpinner from "../loadingSpinner.svelte";
+
+    let edition = $derived(gameMaster.data?.editions[0]?.edition);
 </script>
 
 <Element idName="edition_controller">
-    <button id="change_edition" type="button">{`${gameMaster.data?.editions[0].edition}`}</button>
+    
+    <button id="change_edition" type="button">
+    {#if edition != undefined}
+    {`${edition}`}
+    {:else}
+    <LoadingSpinner/>
+    {/if}
+    </button>
+    
 </Element>
 
 <style>
     :global(#edition_controller) {
-    &._vars {
+    &#edition_controller._vars {
     --border-color: var(--color-element-interactive-border-color);
     --border-radius: 8px;
     --border-weight: 1px;
@@ -21,8 +32,8 @@
     top: 32px;
     left: 16px;
 
-    & button#change_edition { padding: 8px 16px; } 
+    & #change_edition { padding: 8px 16px; } 
 
-    & ._background { background: var(--color-element-interactive-bg); }
+    & #change_edition._background { background: var(--color-element-interactive-bg); }
     }
 </style>
